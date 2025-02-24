4349b1b98246093957fa959d05183abc1a3bcc11
Harkesh Chauhan <harkesh.sain@gmail.com>
2025-02-24 16:07:18 +0000

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Final Update Hook</h1>
+      <h1>Vite + React Github Hook</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
