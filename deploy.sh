7cb07c4b84850a18c0d1c795f752ccda28ab7529
Harkesh Chauhan <harkesh.sain@gmail.com>
2025-02-24 14:26:47 +0000

    deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/frontend.hnweb.site/public_html/apnaportal
-git pull origin main  # Pull latest changes
-npm install           # Install dependencies (optional but recommended)
-npm run build         # Rebuild the project
-systemctl restart lsws  # Restart OpenLiteSpeed for changes to reflect
+git pull origin main
+npm install
+npm run build
+cp -r dist/* /home/frontend.hnweb.site/public_html/
