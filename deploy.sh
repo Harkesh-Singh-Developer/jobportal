d51ced8da6986da64f02b7b2717032bba14d506a
Harkesh Chauhan <harkesh.sain@gmail.com>
2025-02-24 16:05:33 +0000

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+export NVM_DIR="/root/.nvm"
+source $NVM_DIR/nvm.sh
+
 cd /home/frontend.hnweb.site/public_html/apnaportal
 
 git pull origin main
-/usr/bin/npm install
-/usr/bin/npm run build
+/root/.nvm/versions/node/v18.20.5/bin/npm install
+/root/.nvm/versions/node/v18.20.5/bin/npm run build
 
 pm2 restart frontend
 cp -r dist/* /home/frontend.hnweb.site/public_html/
