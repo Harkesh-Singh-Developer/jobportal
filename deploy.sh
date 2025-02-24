ac8ad28949a7c2d35771fd354deefa8bb081990b
Harkesh Chauhan <harkesh.sain@gmail.com>
2025-02-24 16:03:28 +0000

    deply

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd /home/frontend.hnweb.site/public_html/apnaportal
+
 git pull origin main
-npm install
-npm run build
+/usr/bin/npm install
+/usr/bin/npm run build
+
 pm2 restart frontend
 cp -r dist/* /home/frontend.hnweb.site/public_html/
