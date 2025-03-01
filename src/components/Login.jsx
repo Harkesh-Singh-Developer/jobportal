1f31f895b13d712efc2fcbe393e1f8660715c281
Harkesh Singh Chauhan <harkesh.sain@gmail.com>
2025-03-01 18:55:36 +0530

    sync

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -41,7 +41,7 @@ function Login() {
           <Grid size={{ xs: 10, sm: 8, md: 4 }}>
             {/* Using `size` prop for Grid2 */}
             <Card style={{ padding: "20px", textAlign: "center" }}>
-              <CardHeader title={`${loginType} - Login`} />
+              <CardHeader title={`${loginType} - - - Login`} />
               <CardContent>
                 <Grid
                   container
