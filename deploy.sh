49fc04d3ef95f24b4bba7837f7c4b9d60c41ace8
Harkesh Singh Chauhan <harkesh.sain@gmail.com>
2025-03-02 16:00:44 +0530

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,29 @@
 export NVM_DIR="/root/.nvm"
 source $NVM_DIR/nvm.sh
 
-cd /home/frontend.hnweb.site/public_html/apnaportal
+APP_DIR="/home/frontend.hnweb.site/public_html/apnaportal"
+NODE_VERSION="v18.20.5"
+NPM_BIN="/root/.nvm/versions/node/$NODE_VERSION/bin/npm"
 
+cd $APP_DIR
+
+echo "Pulling latest changes from GitHub..."
+git reset --hard origin/main
 git pull origin main
-/root/.nvm/versions/node/v18.20.5/bin/npm install
-/root/.nvm/versions/node/v18.20.5/bin/npm run build
 
-pm2 restart frontend
-# cp -r dist/* /home/frontend.hnweb.site/public_html/
+echo "Clearing node_modules and cache..."
+rm -rf node_modules package-lock.json
+
+echo "Installing dependencies..."
+$NPM_BIN install
+
+echo "Removing old build files..."
+rm -rf dist .next build public
+
+echo "Building application..."
+$NPM_BIN run build
+
+echo "Restarting PM2 with full reload..."
+pm2 restart frontend --update-env
+
+echo "Deployment completed successfully!"
