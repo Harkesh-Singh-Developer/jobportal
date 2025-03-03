2c69a85cba49435e6f6887e141c8a518832ac50a
Harkesh Singh Chauhan <harkesh.sain@gmail.com>
2025-03-03 17:02:21 +0530

    S

--- src/components/seeker/pages/Basicinfo.jsx
+++ src/components/seeker/pages/Basicinfo.jsx
@@ -162,7 +162,7 @@ function Basic_Info() {
               {/* Submit Button */}
               <Grid size={{ xs: 12 }}>
                 <Button variant="contained" color="primary" fullWidth>
-                  Next Step
+                  Next
                 </Button>
               </Grid>
             </Grid>
